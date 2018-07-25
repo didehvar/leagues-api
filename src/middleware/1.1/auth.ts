1cc8a2aaa4727eb651a017e699f1643d8229fab7
James Didehvar <james@didehvar.com>
2018-07-25 09:03:39 +0100

    add v1.1 auth routes

---
+++ src/middleware/1.1/auth.ts
@@ -0,0 +1,80 @@
+import { Middleware } from 'koa';
+import fetch from 'node-fetch';
+
+import log from '../../log';
+import User from '../../models/user';
+import { stravaActivitiesQueue } from '../../queues';
+
+export const refreshToken: Middleware = async ctx => {
+  const user = await User.query().findById(ctx.state.user.id);
+  if (!user) return ctx.throw(404, 'User not found');
+
+  ctx.body = {
+    data: { token: user.jwtToken() },
+  };
+};
+
+export const strava: Middleware = async ctx => {
+  const { code } = ctx.request.body;
+  if (!code) return ctx.throw(400);
+
+  const res = await fetch('https://www.strava.com/oauth/token', {
+    method: 'POST',
+    body: JSON.stringify({
+      client_id: process.env.STRAVA_CLIENT_ID,
+      client_secret: process.env.STRAVA_CLIENT_SECRET,
+      code,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  const {
+    access_token: accessToken,
+    athlete,
+    errors,
+    message,
+  } = await res.json();
+
+  if (!res.ok || !accessToken) {
+    log.warn('Strava token call failed', {
+      status: res.status,
+      errors,
+      message,
+    });
+    return ctx.throw(401, 'Invalid Strava code');
+  }
+
+  let user = await User.query()
+    .where('strava_id', athlete.id)
+    .first();
+
+  if (!user) {
+    user = await User.query()
+      .insert({
+        email: athlete.email,
+        stravaId: athlete.id,
+        stravaAccessToken: accessToken,
+        stravaRaw: athlete,
+        avatar: athlete.profile,
+        firstname: athlete.firstname,
+        lastname: athlete.lastname,
+      })
+      .returning('*');
+
+    stravaActivitiesQueue.add(user);
+  } else {
+    user = await User.query().patchAndFetchById(user.id, {
+      stravaAccessToken: accessToken,
+      stravaRaw: athlete,
+    });
+  }
+
+  ctx.body = {
+    data: {
+      user: {
+        id: user.id,
+      },
+      token: user.jwtToken(),
+    },
+  };
+};
--- src/middleware/1.1/index.ts
+++ src/middleware/1.1/index.ts
@@ -1,6 +1,7 @@
 import * as Router from 'koa-router';
 
 import { leagues, league } from './leagues';
+import { refreshToken, strava } from './auth';
 
 const router = new Router({
   prefix: '/1.1',
@@ -8,5 +9,7 @@ const router = new Router({
 
 router.get('/leagues', leagues);
 router.get('/leagues/:id', league);
+router.get('/auth/refresh', refreshToken);
+router.post('/auth/strava/exchange', strava);
 
 export default router;
