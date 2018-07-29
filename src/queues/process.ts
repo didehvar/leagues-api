b3da04d9c5990c5b94b35fdfbad4bacc497c923c
James Didehvar <james@didehvar.com>
2018-07-29 12:07:44 +0100

    i have no clue

--- src/queues/process.ts
+++ src/queues/process.ts
@@ -1,19 +1,19 @@
 import { resolve } from 'path';
 
 import { stravaQueue, stravaActivitiesQueue } from './index';
+import stravaActivities from './strava-activities';
+import strava from './strava';
 import logger from '../log';
 
 logger.debug('Starting queue processer');
 
-stravaQueue.process(resolve(__dirname, `strava.ts`));
-stravaActivitiesQueue.process(resolve(__dirname, `strava-activities.ts`));
+stravaQueue.process(strava);
+stravaActivitiesQueue.process(stravaActivities);
 
 logger.debug('Queue processer running');
 
 process.on('exit', err => logger.debug('exit event', err));
-process.on('SIGINT', () => logger.debug('SIGINT event'));
-process.on('SIGUSR1', () => logger.debug('SIGUSR1 event'));
-process.on('SIGUSR2', () => logger.debug('SIGUSR2 event'));
-process.on('uncaughtException', err => logger.debug('uncaughtException', err));
-
-process.stdin.resume();
+process.on('uncaughtException', err => {
+  logger.debug('uncaughtException', err);
+  process.exit();
+});
