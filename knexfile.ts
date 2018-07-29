70444eeba6b42f26c536b91c7f23eb0c35ff3296
James Didehvar <james@didehvar.com>
2018-07-29 13:00:27 +0100

    update pool max

--- knexfile.ts
+++ knexfile.ts
@@ -14,7 +14,7 @@ module.exports = {
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
-      max: 10,
+      max: 90,
     },
     migrations: {
       tableName: 'knex_migrations',
