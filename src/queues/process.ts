7a06d186c4241e77e7bc12d4248d48613b8ad2ed
James Didehvar <james@didehvar.com>
2018-07-29 11:49:35 +0100

    log

--- src/queues/process.ts
+++ src/queues/process.ts
@@ -1,6 +1,31 @@
 import { resolve } from 'path';
 
 import { stravaQueue, stravaActivitiesQueue } from './index';
+import logger from '../log';
+
+logger.debug('Starting queue processer');
 
 stravaQueue.process(resolve(__dirname, `strava.ts`));
 stravaActivitiesQueue.process(resolve(__dirname, `strava-activities.ts`));
+
+logger.debug('Queue processer running');
+
+process.once('SIGTERM', () => {
+  logger.debug('Closing all queues before killing worker ...');
+
+  const closePromises: any = [];
+  closePromises.forEach((queue: any) => {
+    closePromises.push(queue.close());
+  });
+
+  Promise.all(closePromises)
+    .then(function() {
+      logger.debug('All queues closed, killing worker');
+      process.exit(0);
+    })
+    .catch(function(err) {
+      logger.debug('Error closing queues, killing worker');
+      logger.error(err);
+      process.exit(0);
+    });
+});
